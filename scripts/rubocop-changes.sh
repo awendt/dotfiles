5f992609d6622f3b4e2afabf62b6e26e13d15bde
André Wendt <awendt@freeshell.org>
2017-03-08 13:43:13 +0100

    Don't run everything when nothing has changed
    rubocop without any args runs through the whole project.

--- scripts/rubocop-changes.sh
+++ scripts/rubocop-changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Script to check modified Ruby files with rubocop, install in any git repo with:
+# Script to check modified Ruby files with rubocop, install in any repo with:
 #
 # cd .git/hooks; ln -s ~/dotfiles/scripts/rubocop-changes.sh pre-commit
 
-rubocop `git diff --cached --name-only --diff-filter=AMR -- *.rb *.rake`
+git diff --cached --name-only --diff-filter=AMR -- *.rb *.rake | xargs rubocop
