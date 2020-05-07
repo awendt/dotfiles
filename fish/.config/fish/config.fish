532ee1788d0a143191bade7bb7588ab544ef0e8f
André Wendt <awendt@freeshell.org>
2020-05-07 08:33:08 +0200

    Link Rubies to Homebrew's SSL
    From `brew upgrade ruby-build`:
    
    ruby-build installs a non-Homebrew OpenSSL for each Ruby version installed and these are never upgraded.
    
    To link Rubies to Homebrew's OpenSSL 1.1 (which is upgraded) add the following
    to your ~/.config/fish/config.fish:
      export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
    
    Note: this may interfere with building old versions of Ruby (e.g <2.4) that use
    OpenSSL <1.1.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,6 @@ status --is-interactive; and source (rbenv init -|psub)
 set -gx LANG en_US.UTF-8
 set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+
+set -gx OPENSSL_DIR (brew --prefix openssl@1.1)
+set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$OPENSSL_DIR"
