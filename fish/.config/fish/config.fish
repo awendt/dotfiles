68554b86fcfeee89e0d253ce561ee725602b8a48
André Wendt <awendt@freeshell.org>
2021-02-05 21:04:14 +0100

    Set ulimit -n 1024 to avoid TF crashing
    Terraform 12 crashes with Error: pipe: too many open files, see
    https://github.com/hashicorp/terraform/issues/23615

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,3 +10,5 @@ set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
 set -gx OPENSSL_DIR (brew --prefix openssl@1.1)
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$OPENSSL_DIR"
+
+ulimit -n 1024
