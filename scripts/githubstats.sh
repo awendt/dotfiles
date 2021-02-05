cff5ae7388cc65037ba85d29f7fef0a23b1de334
André Wendt <awendt@freeshell.org>
2021-02-05 22:32:26 +0100

    githubstats.sh: Introduce --since option
    You can pass a timestamp using --since.
    The macOS `date` implementation is perfectly capable of
    doing what `gdate` did previously (although less elegantly).
    
    So, in process, the script no longer depends on `coreutils`.

--- scripts/githubstats.sh
+++ scripts/githubstats.sh
@@ -1,56 +1,65 @@
 #!/bin/bash
 
-##############################################################
+################################################################
 # Script to count all pull requests that have been assigned
-# the authenticated user in the last week.
-#
-# It depends on coreutils (gdate) and hub.
+# the authenticated user in the last week (requires `hub`).
 #
 # Outputs # of PRs and a list of all Github users
 # that have created those PRs:
 #
-# $ githubstats.sh
-# 4 pull requests:
-# {
-#   "awendt": 1,
-#   "dependabot-preview[bot]": 3
-# }
+#   $ githubstats.sh
+#   4 pull requests:
+#   {
+#     "awendt": 1,
+#     "dependabot-preview[bot]": 3
+#   }
+#
+# It reports since Monday by default, but you can optionally
+# pass a timestamp in the given format:
+#
+#   $ githubstats.sh --since 2021-02-01T00:00:00
 #
 # You can also filter certain usernames:
 #
-# $ githubstats.sh --filter awendt
-# 4 pull requests:
-# {
-#   "awendt": 1
-# }
+#   $ githubstats.sh --filter awendt
+#   4 pull requests:
+#   {
+#     "awendt": 1
+#   }
 #
 # Don't forget to quote when dealing with weird usernames:
 #
-# $ githubstats.sh --filter 'awendt,"dependabot-preview[bot]"'
-# 4 pull requests:
-# {
-#   "awendt": 1,
-#   "dependabot-preview[bot]": 3
-# }
-##############################################################
+#   $ githubstats.sh --filter 'awendt,"dependabot-preview[bot]"'
+#   4 pull requests:
+#   {
+#     "awendt": 1,
+#     "dependabot-preview[bot]": 3
+#   }
+################################################################
 
 # By default, don't filter anything
 filter=. # jq's identity function
 
+# By default, get pull requests since Monday morning
+# * -v is macOS-specific
+# * 1w sets weekday to Monday
+# * zero hours, minutes, and seconds
+since=`date -v 1w -v 0H -v 0M -v 0S +%FT%T`
+
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     --filter) filter="{$2}"; shift ;;
+    --since) since=$2; shift ;;
     *) echo "Unknown parameter passed: $1"; exit 1 ;;
   esac
   shift
 done
 
-MONDAY=`gdate -d'monday -7days' +%FT%T`
-API_URL="https://api.github.com/issues?state=closed&filter=assigned&per_page=100&since=${MONDAY}"
+API_URL="https://api.github.com/issues?state=closed&filter=assigned&per_page=100&since=${since}"
 
 ALL_PRS=`hub api "${API_URL}"`
 
-echo `echo ${ALL_PRS} | jq length` pull requests since ${MONDAY}
+echo `echo ${ALL_PRS} | jq length` pull requests since ${since}
 
 echo ${ALL_PRS} | jq "
   map({title: .title, from: .user.login})
