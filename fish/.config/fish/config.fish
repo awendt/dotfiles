21df15264c67303e02f432581b33bc727cd5156a
André Wendt <awendt@freeshell.org>
2021-02-06 16:23:36 +0100

    Remove sensitive data from .gemrc
    The idea is to have ~/.gemrc.local with credentials and ~/.gemrc for
    all other configuration.
    
    See:
    https://www.soulcutter.com/articles/hiding-gemrc-credentials-in-dotfiles.html
    
    Note: All sources need to be in ~/.gemrc.local, not just the ones
    that require an API token. This is because settings are merged
    all-or-nothing (https://stackoverflow.com/a/35051387/473467)
    
    So ~/.gemrc.local needs to be:
    
        :sources:
        - https://rubygems.org/
        - https://TOKEN@rubygems.pkg.github.com/org/

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,9 @@ set -gx LANG en_US.UTF-8
 set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
+# specific to Ruby
 set -gx OPENSSL_DIR (brew --prefix openssl@1.1)
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$OPENSSL_DIR"
+set -gx GEMRC $HOME/.gemrc.local
 
 ulimit -n 1024
