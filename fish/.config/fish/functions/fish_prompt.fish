4e44378466b597f9417fa5dbd81d1bed3b0671a1
André Wendt <awendt@freeshell.org>
2021-03-18 11:49:15 +0100

    Prompt depends on iTerm profile
    - extract method `last_status_prompt`
    - re-use method for both profiles

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,20 +2,27 @@ set __fish_git_prompt_showcolorhints yes
 set __fish_git_prompt_showdirtystate yes
 set __fish_git_prompt_show_informative_status yes
 
+function last_status_prompt
+  if [ $argv -ne 0 ]
+    set_color red
+    printf "✘"
+  else
+    set_color green
+    printf "✔"
+  end
+  set_color normal
+end
+
 function fish_prompt
   # $status gets nuked as soon as something else is run, e.g. set_color
   # so it has to be saved asap.
   set -l last_status $status
-  if [ $last_status -ne 0 ]
-    set_color red
-    echo -n "✘-$last_status "
-  else
-    set_color green
-    echo -n "✔ "
-  end
-  set_color normal
 
-  printf '%s%s' (prompt_pwd) (__fish_git_prompt)
-  echo
-  printf '%s $ ' (date +%H:%M)
+  if [ "$ITERM_PROFILE" = "terrabits" ]
+    printf '%s%s $ ' (last_status_prompt $last_status) (__fish_git_prompt)
+  else
+    printf '%s %s%s' (last_status_prompt $last_status) (prompt_pwd) (__fish_git_prompt)
+    echo
+    printf '%s $ ' (date +%H:%M)
+  end
 end
