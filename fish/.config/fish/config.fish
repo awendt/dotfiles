446d45cba665a3f091874fc08f40f6f434316195
André Wendt <awendt@freeshell.org>
2023-01-13 09:56:19 +0100

    Fix brew's OPENSSL_DIR

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,7 +15,7 @@ set -g fish_user_paths "/usr/local/opt/node@14/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
 # specific to Ruby
-set -gx OPENSSL_DIR (brew --prefix openssl@1.1)
+set -gx OPENSSL_DIR (brew --prefix openssl)
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$OPENSSL_DIR"
 set -gx GEMRC $HOME/.gemrc.local
 
